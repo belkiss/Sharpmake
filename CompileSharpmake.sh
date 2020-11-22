6c5386dc9fba4d0a03781d3f7134691b2973641e
Lambert Clara <lambert.clara@yahoo.fr>
2020-11-22 12:30:53 +0100

    dotnet build

--- CompileSharpmake.sh
+++ CompileSharpmake.sh
@@ -10,9 +10,11 @@ function BuildSharpmake {
     configuration=$2
     platform=$3
     echo Compiling $solutionPath in "${configuration}|${platform}"...
-    MSBUILD_CMD="msbuild -t:build -restore \"${solutionPath}\" /nologo /v:m /p:Configuration=${configuration} /p:Platform=\"${platform}\""
-    echo $MSBUILD_CMD
-    eval $MSBUILD_CMD
+
+    BUILD_CMD="dotnet build \"$solutionPath\" -v m -nologo -c \"$configuration\""
+
+    echo $BUILD_CMD
+    eval $BUILD_CMD
     if [ $? -ne 0 ]; then
         echo ERROR: Failed to compile $solutionPath in "${configuration}|${platform}".
         exit 1
@@ -24,11 +26,11 @@ set -e
 
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-which msbuild > /dev/null
-MSBUILD_FOUND=$?
-if [ $MSBUILD_FOUND -ne 0 ]; then
-    echo "MSBuild not found"
-    exit $MSBUILD_FOUND
+which dotnet > /dev/null
+DOTNET_FOUND=$?
+if [ $DOTNET_FOUND -ne 0 ]; then
+    echo "dotnet not found, see https://dotnet.microsoft.com/download"
+    exit $DOTNET_FOUND
 fi
 
 SOLUTION_PATH=${1:-"${CURRENT_DIR}/Sharpmake.sln"}
